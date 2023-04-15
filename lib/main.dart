4cb9481c865b9bbb90d167d72ab322981a7aa1ae
Md. Asikuzzaman <jamanasik@gmail.com>
2023-04-16 03:38:34 +0600

    Asik: web run done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instaclone/Responsive/mobile_Screen_Layout.dart';
 import 'package:instaclone/Responsive/responsive_Screen_Layout.dart';
@@ -7,7 +8,18 @@ import 'package:instaclone/Utils/customColor.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: "AIzaSyDn4JL05GVHRJQHdxUOU_y--8Kp-Iy5oyc",
+            appId: "1:880379209599:web:641596b8cf1482baacdc4a",
+            messagingSenderId: "880379209599",
+            projectId: "instaclone-5c69a",
+            storageBucket: "instaclone-5c69a.appspot.com"));
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
